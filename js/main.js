875eb4d52881e419510b21571a1f9fff30391032
igorpereira28 <igorsilvape28@gmail.com>
2021-06-01 02:06:02 -0300

    Jogo funcional finalizado

--- js/main.js
+++ js/main.js
@@ -46,6 +46,13 @@ function iniciarJogo() {
     if (snake[0].y > 15 * box && direcao == "down") snake[0].y = 0;
     if (snake[0].y < 0 && direcao == "up") snake[0].y = 16 * box;
 
+    for (i = 1; i < snake.length; i++) {
+        if (snake[0].x == snake[i].x && snake[0].y == snake[i].y) {
+            clearInterval(jogo);
+            alert('Game Over ):');
+        }
+    }
+
     criarBG();
     criarCobrinha();
     drawFood();
